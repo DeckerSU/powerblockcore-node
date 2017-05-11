432cc9be3c53ffd82a5d27f11a8b672c8d7e2669
losh11 <loshan1212@gmail.com>
2017-05-11 03:36:55 +0100

    Bump to 3.1.9

--- regtest/node.js
+++ regtest/node.js
@@ -15,7 +15,7 @@ var node;
 
 var should = chai.should();
 
-var BitcoinRPC = require('bitcoind-rpc');
+var BitcoinRPC = require('litecoind-rpc');
 var index = require('..');
 var Transaction = bitcore.Transaction;
 var BitcoreNode = index.Node;
